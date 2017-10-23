9759498eb29e14d5188dff8d01bf7231773d4687
Greg Lawrence <gregaudio@gmail.com>
2017-10-23 13:13:11 -0500

    created cards now include the user submited text from text area. Delete button on each card works to delete just that card.

--- cards.js
+++ cards.js
@@ -11,19 +11,40 @@ let idFactory = uniqueIdGen();
 const createCardBtnEl = document.getElementById("createCardBtn");
 
 let createCard = function(event) {
-    console.log(event);
+    // console.log(event);
     let cardsEl = document.getElementById("cards");
     let currentCardId = idFactory.next().value;
-
+    let cardContent = document.getElementById("card-content");
+    console.log(cardContent)
     cardsEl.innerHTML += `
         <article class="card" id="article_${currentCardId}">
-            <p>I'm a new card</p>
+            <p>${cardContent.value}</p>
             <button id="button_${currentCardId}">Delete Me</button>
         </article>
         `
+    cardContent.value = "";    
+}
+
+createCardBtnEl.addEventListener("click", createCard)
+
+const cardsEl = document.getElementById("cards")
+
+let deleteCard = function(event) {
+    console.log("deleteCard event: ", event);
+    // console.log("btnId: ", event.target.id.split("_")[1]);
+    const cardsEl = document.getElementById("cards");
+    const btnId = event.target.id
+    const btnIdNum = btnId.split("_")[1];
+
+    if (btnId.startsWith("button_")) {
+        // console.log("yes it starts with button_")
+        let cardToRemove = document.getElementById("article_" + btnIdNum)
+        cardsEl.removeChild(cardToRemove)
+
         
+    }
 }
 
 
 
-createCardBtnEl.addEventListener("click", createCard)
+cardsEl.addEventListener("click", deleteCard)
